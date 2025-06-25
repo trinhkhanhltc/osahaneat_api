74f92c565237165fb4f7b09837a0fc1bfc49a144
trinhkhanhltc <trinhkhanhltc@gmail.com>
2025-06-25 10:09:58 +0700

    Initial commit

---
+++ src/main/java/com/khanhtt/osahaneat/OsahaneatApplication.java
@@ -0,0 +1,13 @@
+package com.khanhtt.osahaneat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsahaneatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsahaneatApplication.class, args);
+	}
+
+}
